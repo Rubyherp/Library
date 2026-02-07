ea75bc268e15df376da9b252f5096a935952ddbc
XN <horxiangneng123@gmail.com>
2026-02-07 19:49:15 +0800

    complete simple library with add drop functions

--- script.js
+++ script.js
@@ -1,57 +1,152 @@
-
 const cardsContainer = document.querySelector(".cards");
-const formWrapper = document.querySelector("#form-wrapper")
+const formWrapper = document.querySelector("#form-wrapper");
 const addBookForm = document.querySelector(".post-form");
 const closeTab = document.querySelector(".close-btn");
+const submitBtn = document.querySelector(".submit-btn");
 
 addBookForm.addEventListener("click", () => {
-    formWrapper.classList.remove("hidden"); // remove hidden to show
+  formWrapper.classList.remove("hidden"); // remove hidden to show
 });
 
 closeTab.addEventListener("click", () => {
-    formWrapper.classList.add("hidden"); // add hidden to hide
+  formWrapper.classList.add("hidden"); // add hidden to hide
 });
 
 formWrapper.addEventListener("click", (e) => {
-    if (e.target === formWrapper) {
-        formWrapper.classList.add("hidden");
-    }
+  if (e.target === formWrapper) {
+    formWrapper.classList.add("hidden");
+  }
 });
+
+submitBtn.addEventListener("click", () => {
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const status = document.querySelector("input[name='status']:checked");
+
+  if (title && author && status) {
+    const newBook = new Book(title, author, status.value);
+    addBookToLibrary(newBook);
+
+    createCard(newBook);
+
+    // Clear form
+    document.querySelector(".form-details").reset();
+
+    // Close form
+    formWrapper.classList.add("hidden");
+
+  } else {
+    alert("Please fill in all fields");
+  }
+});
+
+cardsContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("card-remove")) {
+        const card = e.target.closest(".card");
+        card.remove();
+    }
+})
 
 const myLibrary = [];
 
 class Book {
-    constructor(title, id) {
-        this.title = title;
-        this.id = id;
-    }
+  constructor(title, author, status) {
+    this.title = title;
+    this.author = author;
+    this.status = status;
+  }
 }
 
-function addBookToLibrary(title) {
-    let newBook = new Book(title, crypto.randomUUID())
-    myLibrary.push(newBook);
-    console.log(`Book: ${title}, added to Library`);
+function createCard(book) {
+
+  // create new div element to add to HTML
+  const div = document.createElement("div");
+  const title = document.createElement("div");
+  const author = document.createElement("div");
+  const status = document.createElement("div");
+  const removeBtn = document.createElement("button");
+
+  div.classList.add("card");
+  div.appendChild(title);
+  title.classList.add("card-title");
+  div.appendChild(author);
+  author.classList.add("card-author");
+  div.appendChild(status);
+  status.classList.add("card-status");
+  div.appendChild(removeBtn);
+  removeBtn.classList.add("card-remove");
+
+  let tempStatus = "";
+  if (book.status === "1") {
+    status.classList.add("unread");
+    tempStatus = "UNREAD";
+  } else if (book.status === "2") {
+    status.classList.add("reading");
+    tempStatus = "READING";
+  } else if (book.status === "3") {
+    status.classList.add("finished");
+    tempStatus = "FINISHED";
+  }
+
+  title.textContent = book.title;
+  author.textContent = book.author;
+  status.textContent = tempStatus;
+  removeBtn.textContent = "Remove";
+
+  // Make status clickable to change it
+  status.style.cursor = "pointer";
+  status.addEventListener("click", () => {
+    cycleStatus(book, status);
+  });
+
+  cardsContainer.appendChild(div);
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function cycleStatus(book, statusElement) {
+  if (book.status === "1") {
+    book.status = "2";
+  } else if (book.status === "2") {
+    book.status = "3";
+  } else if (book.status === "3") {
+    book.status = "1";
+  }
+
+  statusElement.classList.remove("unread", "reading", "finished");
+  
+  let tempStatus = "";
+  if (book.status === "1") {
+    statusElement.classList.add("unread");
+    tempStatus = "UNREAD";
+  } else if (book.status === "2") {
+    statusElement.classList.add("reading");
+    tempStatus = "READING";
+  } else if (book.status === "3") {
+    statusElement.classList.add("finished");
+    tempStatus = "FINISHED";
+  }
+
+  statusElement.textContent = tempStatus;
 }
 
 function displayBook() {
-    myLibrary.forEach((book) => {
-
-        // create new div element to add to HTML
-        const div = document.createElement("div");
-        div.classList.add("card");
-        div.textContent = book.title;
-        cardsContainer.appendChild(div);
-
-        console.log("Title: " + book.title);
-        console.log("Id: " + book.id);
-    });
+  cardsContainer.innerHTML = '';
+  myLibrary.forEach((book) => {
+    createCard(book);
+  });
 }
 
-function addNewBook() {
-}
+const book1 = new Book("Harry Potter", "JK Rowling", "3");
+const book2 = new Book("Xiang Neng's Magical Journey", "Xiang Neng", "2");
+const book3 = new Book("Xiang Neng's files!?", "FBI", "1");
+const book4 = new Book("Exploring XN's Island", "FBI", "1");
 
-addBookToLibrary("Harry Potter");
-addBookToLibrary("Intro to JS");
-addBookToLibrary("C++ for Beginners");
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+addBookToLibrary(book4);
 
 displayBook();
